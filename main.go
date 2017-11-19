47f8d912d480ded05fbf5dd0b1cab11de3831d04
vitaminwater <stantin04@gmail.com>
2017-11-19 19:41:26 +0100

    started adding rpc method proxyfiing

--- main.go
+++ main.go
@@ -1,26 +1,120 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/vitaminwater/daryl/protodef"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
-	"github.com/vitaminwater/daryl_db"
 )
 
-type Test struct {
-	Id       int    `db:"id" access:"s"`
-	Slug     string `db:"slug" access:"i,s"`
-	UserName string `db:"username" access:"i,u,s"`
+const AUTH_TOKEN_HEADER = "X-Daryl-Auth-Token"
+
+func userMessage(c *gin.Context) {
+	url := c.MustGet("daryl_url")
+	m := protodef.Message{}
+	if err := c.BindJSON(&m); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	log.Info(url, m)
+	c.JSON(200, gin.H{
+		"status": "posted",
+	})
 }
 
-func init() {
-	daryl_db.Init()
+func addHabit(c *gin.Context) {
+	url := c.MustGet("daryl_url")
+	h := protodef.Habit{}
+	if err := c.BindJSON(&h); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	log.Info(url, h)
+	c.JSON(200, gin.H{
+		"status": "posted",
+	})
 }
 
-func main() {
-	test := Test{Slug: daryl_db.UUID(), UserName: "lolname"}
-	err := daryl_db.Insert("primate", &test)
+func startWorkSession(c *gin.Context) {
+	url := c.MustGet("daryl_url")
+	s := protodef.StartWorkSessionRequest{}
+	if err := c.BindJSON(&s); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	log.Info(url, s)
+	c.JSON(200, gin.H{
+		"status": "posted",
+	})
+}
+
+func cancelWorkSession(c *gin.Context) {
+	url := c.MustGet("daryl_url")
+	log.Info(url)
+	c.JSON(200, gin.H{
+		"status": "posted",
+	})
+}
+
+func refuseWorkSession(c *gin.Context) {
+	url := c.MustGet("daryl_url")
+	log.Info(url)
+	c.JSON(200, gin.H{
+		"status": "posted",
+	})
+}
+
+func findDarylServer(identifier string) (string, error) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	test.UserName = "lolname2"
-	daryl_db.Update("primate", "id", test)
+	defer cli.Close()
+
+	resp, err := cli.Get(context.Background(), fmt.Sprintf("daryl_%s", identifier))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(resp.Kvs) != 0 {
+		url := string(resp.Kvs[0].Value)
+		log.Info(url)
+		return url, nil
+	}
+	return "", errors.New("Daryl not found")
+}
+
+func setDarylServer(c *gin.Context) {
+	url, err := findDarylServer("lol")
+	if err != nil {
+		c.JSON(http.StatusServiceUnavailable, gin.H{"error": err})
+		return
+	}
+	log.Infof("Daryl at %s", url)
+	c.Set("daryl_url", url)
+	c.Next()
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(setDarylServer)
+	router.POST("/message", userMessage)
+	router.POST("/habit", addHabit)
+
+	session := router.Group("/session")
+	{
+		session.POST("/", startWorkSession)
+		session.POST("/cancel", cancelWorkSession)
+		session.GET("/refuse", refuseWorkSession)
+	}
+	router.Run()
 }
